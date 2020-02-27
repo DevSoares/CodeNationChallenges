427afbf327af461a17cdda11f7a305e755195a71
Luiz Soares <dev.soares@outlook.com>
2020-02-26 23:08:50 -0300

    Implemented UserService methods

--- csharp-8/Source/Services/UserService.cs
+++ csharp-8/Source/Services/UserService.cs
@@ -1,32 +1,61 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Codenation.Challenge.Models;
 
 namespace Codenation.Challenge.Services
 {
     public class UserService : IUserService
     {
+        private readonly CodenationContext CodenationContext;
         public UserService(CodenationContext context)
         {
+            CodenationContext = context;
         }
 
         public IList<User> FindByAccelerationName(string name)
         {
-            throw new System.NotImplementedException();
+            List<int> acceletarionIds = CodenationContext.Accelerations.Where(a => a.Name == name).Select(a => a.Id).ToList();
+
+            IList<User> users = new List<User>();
+
+            Parallel.ForEach(acceletarionIds, action =>
+            {
+                users.Add(FindById(acceletarionIds.FirstOrDefault()));
+            });
+
+            return users;
         }
 
         public IList<User> FindByCompanyId(int companyId)
         {
-            throw new System.NotImplementedException();
+            List<int> userIds = CodenationContext.Candidates.Where(c => c.CompanyId == companyId).Select(c2 => c2.UserId).ToList();
+            IList<User> users = new List<User>();
+
+            Parallel.ForEach(userIds, action =>
+            {
+                users.Add(FindById(userIds.FirstOrDefault()));
+            });
+
+            return users;
         }
 
         public User FindById(int id)
-        {            
-            throw new System.NotImplementedException();
+        {
+            return CodenationContext.Users.FirstOrDefault(u => u.Id == id);
         }
 
         public User Save(User user)
         {
-            throw new System.NotImplementedException();
+            if (user.Id == 0)
+                CodenationContext.Users.Add(user);
+            else
+                CodenationContext.Users.Update(user);
+
+            CodenationContext.SaveChanges();
+
+            return user;
         }
     }
 }
