b227750b81729b1e2836480b30c16e3956843212
Luiz Soares <dev.soares@outlook.com>
2020-02-27 21:25:43 -0300

    Implemented CompanyService methods

--- csharp-8/Source/Services/CompanyService.cs
+++ csharp-8/Source/Services/CompanyService.cs
@@ -1,32 +1,61 @@
 using System.Collections.Generic;
 using Codenation.Challenge.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Codenation.Challenge.Services
 {
     public class CompanyService : ICompanyService
     {
+        private readonly CodenationContext CodenationContext;
         public CompanyService(CodenationContext context)
         {
+            CodenationContext = context;
         }
 
         public IList<Company> FindByAccelerationId(int accelerationId)
         {
-            throw new System.NotImplementedException();
+            List<int> companiesIds = CodenationContext.Candidates.Where(c => c.AccelerationId == accelerationId).Select(a => a.CompanyId).ToList();
+
+            IList<Company> companies = new List<Company>();
+
+            Parallel.ForEach(companiesIds, action =>
+            {
+                companies.Add(FindById(companiesIds.FirstOrDefault()));
+            });
+
+            return companies;
         }
 
         public Company FindById(int id)
         {
-            throw new System.NotImplementedException();
+            return CodenationContext.Companies.Find(id);
         }
 
         public IList<Company> FindByUserId(int userId)
         {
-            throw new System.NotImplementedException();
+            List<int> companiesIds = CodenationContext.Candidates.Where(c => c.UserId == userId).Select(a => a.CompanyId).ToList();
+
+            IList<Company> companies = new List<Company>();
+
+            Parallel.ForEach(companiesIds, action =>
+            {
+                companies.Add(FindById(companiesIds.FirstOrDefault()));
+            });
+
+            return companies;
         }
 
         public Company Save(Company company)
         {
-            throw new System.NotImplementedException();
+            if (company.Id == 0)
+                CodenationContext.Companies.Add(company);
+            else
+                CodenationContext.Companies.Update(company);
+
+            CodenationContext.SaveChanges();
+
+            return company;
         }
     }
 }
