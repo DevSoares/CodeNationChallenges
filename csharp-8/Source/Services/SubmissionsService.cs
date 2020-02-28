30ed74d37e01506b28317f7d3f03405d473287ca
Luiz Soares <dev.soares@outlook.com>
2020-02-27 22:16:15 -0300

    Implemented SubmissionsService methods

--- csharp-8/Source/Services/SubmissionsService.cs
+++ csharp-8/Source/Services/SubmissionsService.cs
@@ -1,27 +1,37 @@
 using System.Collections.Generic;
+using System.Linq;
 using Codenation.Challenge.Models;
 
 namespace Codenation.Challenge.Services
 {
     public class SubmissionService : ISubmissionService
     {
+        private readonly CodenationContext CodenationContext;
         public SubmissionService(CodenationContext context)
         {
+            CodenationContext = context;
         }
 
         public IList<Submission> FindByChallengeIdAndAccelerationId(int challengeId, int accelerationId)
         {
-            throw new System.NotImplementedException();
+            List<int> challengeIds = CodenationContext.Accelerations.Where(a => a.Id == accelerationId).Select(a => a.ChallengeId).ToList();
+
+            return CodenationContext.Submissions.Where(s => challengeIds.Contains(s.ChallengeId)).ToList();
         }
 
         public decimal FindHigherScoreByChallengeId(int challengeId)
         {
-            throw new System.NotImplementedException();
+            return CodenationContext.Submissions.Where(s => s.ChallengeId == challengeId).Select(s => s.Score).Max();
         }
 
         public Submission Save(Submission submission)
         {
-            throw new System.NotImplementedException();
+            if ((submission.UserId == 0) && (submission.ChallengeId == 0))
+                CodenationContext.Submissions.Add(submission);
+            else
+                CodenationContext.Submissions.Update(submission);
+
+            return submission;
         }
     }
 }
